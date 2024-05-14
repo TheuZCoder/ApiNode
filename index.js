3726ed6f7d8ea0adc2da415faa1912c639b7e201
TheuZCoder <matheusrdsilva1@gmail.com>
2024-05-14 14:58:16 -0300

    external connection

--- index.js
+++ index.js
@@ -15,11 +15,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_DATABASE,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false } // Adicione esta linha se estiver usando SSL/TLS
 });
 
 async function criarTabelas() {
